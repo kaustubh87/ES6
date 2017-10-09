d685b9d388057fff66458af6ad1b8db7281c9f24
kvin1087 <kaustubh87@gmail.com>
2017-10-08 21:36:25 -0500

    Added Class definition

---
+++ class.js
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/babel-core/5.8.29/browser.js"></script>
+    
+    <script type="text/babel">
+        class Vehicle{
+            constructor(description, wheels){
+                this.description = description;
+                this.wheels = wheels;
+            }
+        }
+        </script>
+        <title>Classes</title>
+</head>
+
+<body>
+<section>
+
+</section>
+</body>
+
+</html>
